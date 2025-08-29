6444d33a4d4a4a215004928cc1734fda90fa674c
Khandkar-Sajiduzzaman <khandkar.sajid@gmail.com>
2025-08-30 01:58:10 +0600

    feat: created tables in DB

--- supabase/migrations/20250829161921_createFaculty.sql
+++ supabase/migrations/20250829161921_createFaculty.sql
@@ -2,5 +2,8 @@ CREATE TABLE faculty (
     facultyID UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     facultyName TEXT NOT NULL,
     email TEXT NOT NULL,
-    imgURL TEXT
+    imgURL TEXT,
+
+    CONSTRAINT valid_img_url
+       CHECK (imgURL LIKE 'https://%')
 );
---
+++ supabase/migrations/20250829185707_createModeratesReview.sql
@@ -0,0 +1,25 @@
+CREATE TYPE decision AS ENUM ('pending', 'published', 'rejected');
+
+CREATE TABLE moderatesReview (
+    reviewID UUID NOT NULL,
+    moderatorEmail TEXT NOT NULL,
+    moderatedAt BIGINT DEFAULT EXTRACT(EPOCH FROM NOW())::BIGINT,
+    comment text NOT NULL,
+    decisionState decision NOT NULL,
+
+    PRIMARY KEY (reviewID, moderatorEmail,moderatedAt),
+    
+
+    CONSTRAINT fk_review_ID
+        FOREIGN KEY (reviewID) 
+        REFERENCES reviews(reviewID) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+
+    CONSTRAINT fk_moderator_email
+        FOREIGN KEY (moderatorEmail) 
+        REFERENCES userinfo(email) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+        
+);
