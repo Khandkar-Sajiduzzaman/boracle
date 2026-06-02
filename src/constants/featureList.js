30356f06ab00873e4667c53e9af462d26cb4dd4c
eniamza <eniamza0@gmail.com>
2026-06-02 21:08:14 +0600

    chore: update gradesheet analyzer for homepage

--- src/constants/featureList.js
+++ src/constants/featureList.js
@@ -35,9 +35,11 @@ const featureList = [
     },
     {
         index: 5,
-        title: "Faculty Review",
-        description: "Rate your faculty and provide feedback. Scrolling through useless reviews is a thing of the past!",
-        footer: "🟡 Coming Soon"
+        title: "Gradesheet Analyzer",
+        description: "Calculate Retakes, See Grade Tolerances and Beautiful Charts!",
+        footer: "🟢 Live",
+        href: "/gradesheet",
+        dashboardHref: "/dashboard/gradesheet"
     },
 ]
 
